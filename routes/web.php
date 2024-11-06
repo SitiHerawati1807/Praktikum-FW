6a0f1b771d5cdab1d7b34bc508cbb1e403c41c92
SitiHerawati1807 <2210631170153@student.unsika.ac.id>
2024-11-06 11:20:06 +0700

    pertemuan sembilan

--- routes/web.php
+++ routes/web.php
@@ -1,68 +1,45 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SupplierController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'hello word!';
-});
-
-Route::get('/user/{id}', function ($id) {
-    return 'User ID:' .$id; 
-});
-
-
-Route::get('/user/{name?}', function ($name = 'Guest') {
-    return 'Hello,' .$name; 
-});
-
-Route::get('/profile', function () {
-    return 'This is the profile page.'; 
-})->name('profile');
-
-
-Route::get('/redirect-to-profile', function () {
-    return redirect()->route('profile'); 
-});
-
-Route::get('/user/{name?}', function ($name = 'Guest') {
-    return 'Hello,' .$name; 
-});
-
-Route::get('/profile', function () {
-    return 'This is the profile page.'; 
-})->name('profile');
-
-
-Route::prefix('admin')->group(function(){
 Route::get('/dashboard', function () {
-    return 'Admin Dashboard'; 
-    });
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/profile', function () {
-    return 'Admin profile'; 
-    });
-
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+require __DIR__ . '/auth.php';
 
 Route::get('/dashboard', function () {
-    return 'Welcome to your dashboard!'; 
-})->middleware('auth');
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'RoleCheck:admin'])->name('dashboard');
 
 
-Route::resource('post', 'PostController');
+
+Route::get('/products', [ProductController::class, 'index'])->name('product-index');
+Route::get('/product/create', [ProductController::class, 'create'])->name('product-create');
+Route::post('/product', [ProductController::class, 'store'])->name('product-store');
+Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product-edit');
+Route::put('/product/{id}', [ProductController::class, 'update'])->name('product-update');
+Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product-deleted');
 
 
-Route::get('/penjumlahan/1/2', function () {
-    return '1+2=3';
-});
-
-
-Route::get('/penjumlahan/{angka1}/{angka2}', function ($angka1, $angka2) {
-    $hasil = $angka1 + $angka2;
-    return 'Hasil Penjumlahan: ' . $hasil;
-});
+Route::get('/supplier', [SupplierController::class, 'index']);
+Route::get('/supplier/create', [SupplierController::class, 'create'])->name("supplier-create");
+Route::post('/supplier', [SupplierController::class, 'store'])->name("supplier-store");
+Route::get('/supplier/{id}', [SupplierController::class, 'show']);
+Route::get('/supplier/{id}/edit', [SupplierController::class, 'edit']);
+Route::put('/supplier/{id}', [SupplierController::class, 'update']);
+Route::delete('/supplier/{id}', [SupplierController::class, 'destory']);
